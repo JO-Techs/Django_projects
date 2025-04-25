b128cbbd35c1019305e16e31aefb9290099f03b0
JOEL TITO <157946811+JO-Techs@users.noreply.github.com>
2025-04-25 13:40:15 +0530

    Add files via upload

---
+++ Notifier/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import FailureLog, NotificationRule
+
+class FailureLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FailureLog
+        fields = '__all__'
+
+class NotificationRuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NotificationRule
+        fields = '__all__'
+
+class DummyAPI1Serializer(serializers.Serializer):
+    age = serializers.IntegerField(required=True, min_value=1, error_messages={
+        "required": "The 'age' field is required.",
+        "invalid": "The 'age' field must be a valid integer.",
+        "min_value": "The 'age' field must be greater than 0."
+    })
+
+class DummyAPI2Serializer(serializers.Serializer):
+    field_value = serializers.IntegerField(required=True, error_messages={
+        "required": "The 'field_value' field is required.",
+        "invalid": "The 'field_value' field must be a valid integer."
+    })
